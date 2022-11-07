c56c2c9c0a39d694e196a765cadb3bc79ae16a03
Hunter Belanger <hunter.belanger@gmail.com>
2022-11-07 15:47:25 +0100

    Adds version.hpp and CMakeLists

---
+++ include/PapillonGeom/version.hpp
@@ -0,0 +1,41 @@
+/*
+ * Papillon Geometry Library
+ * Copyright 2022, Hunter Belanger
+ *
+ * hunter.belanger@gmail.com
+ *
+ * This file is part of the Papillon Geometry Library (PapillonGeom).
+ *
+ * PapillonGeom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PapillonGeom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PapillonGeom. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * */
+#ifndef PAPILLON_GEOM_VERSION_H
+#define PAPILLON_GEOM_VERSION_H
+
+/**
+ * @file
+ * @author Hunter Belanger
+ */
+
+namespace pgeom {
+
+extern const unsigned int VERSION_MAJOR;
+extern const unsigned int VERSION_MINOR;
+extern const unsigned int VERSION_PATCH;
+extern const bool VERSION_DEVELOPMENT;
+extern const char* VERSION_STRING;
+
+}  // namespace pgeom
+
+#endif
---
+++ src/version.cpp
@@ -0,0 +1,39 @@
+/*
+ * Papillon Geometry Library
+ * Copyright 2022, Hunter Belanger
+ *
+ * hunter.belanger@gmail.com
+ *
+ * This file is part of the Papillon Geometry Library (PapillonGeom).
+ *
+ * PapillonGeom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PapillonGeom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PapillonGeom. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * */
+
+/**
+ * @file
+ * @author Hunter Belanger
+ */
+
+#include <PapillonGeom/version.hpp>
+
+namespace pgeom {
+
+const unsigned int VERSION_MAJOR = 0;
+const unsigned int VERSION_MINOR = 1;
+const unsigned int VERSION_PATCH = 0;
+const bool VERSION_DEVELOPMENT = true;
+const char* VERSION_STRING = "0.1.0-dev";
+
+}  // namespace pgeom
